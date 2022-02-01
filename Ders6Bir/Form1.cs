a82c4c20d2a8677e1c008965f8e6fd877a41972d
pinar <pinar@DESKTOP-1E2A08D>
2022-02-02 02:11:41 +0300

    Proje dosyası ekle.

---
+++ Ders6Bir/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ders6Bir
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            int say1 = r.Next(1, 3);
+            int say2 = r.Next(1, 3);
+            int say3 = r.Next(1, 3);
+            label1.Text = say1.ToString();
+            label2.Text = say2.ToString();
+            label3.Text = say3.ToString();
+            if (say1 == say2 & say2 == say3) MessageBox.Show("BİNGO");
+        }
+    }
+}
